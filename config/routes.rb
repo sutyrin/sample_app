ef8b35e34d83fd4bbcc8d1b43cd2fdcca3c4a469
Paul Sutyrin <paul.sutyrin@gmail.com>
2011-11-29 03:15:06 +0400

    some nea trroutes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 SampleApp::Application.routes.draw do
+  # route for static
+
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+  root :to => "pages#home"
+
   get "pages/home"
   get "pages/about"
   get "pages/contact"
@@ -53,7 +60,6 @@ SampleApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
 
   # See how all your routes lay out with "rake routes"
 
