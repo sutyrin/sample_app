a649c88d2023da72a27763a534254de46a84ad0f
Paul Sutyrin <paul.sutyrin@gmail.com>
2011-11-29 03:42:11 +0400

    logo helpe

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -32,6 +32,8 @@ describe "LayoutLinks" do
     response.should have_selector('title', :content => "Contact")
     click_link "Home"
     response.should have_selector('title', :content => "Home")
+    click_link "Sign up now!"
+    response.should have_selector('title', :content => "Sign up")
   end
 
 end
