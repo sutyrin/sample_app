4da65b0a91382e70488c1f10c22b5cb2bf96b4dd
Paul Sutyrin <paul.sutyrin@gmail.com>
2011-12-18 14:33:55 +0400

    added factory for user

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -51,7 +51,4 @@ describe PagesController do
     end
   end
 
-
-
-
 end
---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+   user.name                      "Booba Kropotkinsky"
+   user.email                     "user@internet.com"
+   user.password                  "sekretos"
+   user.password_confirmation     "sekretos" 
+end
